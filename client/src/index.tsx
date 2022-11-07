36acd672f29578f3c9d085688576d9022874c7a0
marbal88 <109217383+marbal88@users.noreply.github.com>
2022-11-07 18:02:37 +0100

    End of section 6

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from "history";
+import { StoreProvider } from './app/context/StoreContext';
+import App from './app/layout/App';
 
 export const history = createBrowserHistory();
 
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router history={history}>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </Router>
   </React.StrictMode>
 );
